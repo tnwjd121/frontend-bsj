9c3f94643e308d54ef7cea236f01ae04e0a4559b
tnwjd121 <aojj121@gmail.com>
2024-01-09 12:17:15 +0900

    학습완료

--- react0912/src/App.tsx
+++ react0912/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-// 파일명을 index로 지정하는 경우 해당 파일이 존재하는 디렉토리(폴더)의 기본(메인) 파일임을 파일명으로 알려줌.
+// 파일명을 index로 지정하는 경우
+// : 웹 페이지의 메인 파일 - 해당 파일이 존재하는 디렉토리(폴더)의 기본(메인) 파일임을 파일명으로 알려줌.
 import Index1219 from "./views/1219";
 import Index1221 from "./views/1221";
 import Index1222 from "./views/1222";
@@ -8,28 +9,46 @@ import Index1228 from "./views/1228";
 import Index1229 from "./views/1229/react";
 import Index0102 from "./views/0102";
 import Index0104 from "./views/0104";
+import Index0105 from "./views/0105"
+
+import ThemeProvider from "./views/0105/a_ContextAPI/ThemeProvider";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
   return (
     <>
-      <Routes>
-        {/*
-          npm run start
-          기본 경로 - localhost: 3000
-          Route의 path속성으로 지정된 경로로 element가 렌더링
-          npm run start의 첫번째 렌더링 페이지의 경우 경로를 "/" 전달 
-         */}
-        {/* <Route path="/" element={<렌더링 할 페이지 />} /> */}
-        <Route path="/1219" element={<Index1219 />}/>
-        <Route path="/1221" element={<Index1221 />}/>
-        <Route path="/1222" element={<Index1222 />}/>
-        <Route path="/1226" element={<Index1226 />}/>
-        <Route path="/1228" element={<Index1228 />}/>        
-        <Route path="/1229" element={<Index1229 />}/>        
-        <Route path="/0102" element={<Index0102 />}/>    
-        <Route path="/0104" element={<Index0104 />}/>         
+      <GlobalStyle />
+      <ThemeProvider>
+        <Routes>
+          {/* 
+            npm run start
+            기본 경로 - localhost: 3000
+            Route의 path속성으로 지정된 경로로 element가 렌더링
 
-      </Routes>
+            npm run start의 첫번째 렌더링 페이지의 경우 경로를 "/" 전달
+          */}
+          {/* 
+          <Route path="/" element={<렌더링 할 페이지 />} /> 
+          */}
+          <Route path="/1219" element={<Index1219 />} />
+          <Route path="/1221" element={<Index1221 />} />
+          <Route path="/1222" element={<Index1222 />} />
+          <Route path="/1226" element={<Index1226 />} />
+          <Route path="/1228" element={<Index1228 />} />
+          <Route path="/1229" element={<Index1229 />} />
+          <Route path="/0102" element={<Index0102 />} />
+          <Route path="/0104" element={<Index0104 />} />
+          <Route path="/0105" element={<Index0105 />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
---
+++ react0912/src/views/0105/index.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import ContextApi01 from './a_ContextAPI/ContextApi01';
+import { ThemeContext } from './a_ContextAPI/ThemeContext';
+import Styled01 from './b_StyledComponents/Styled01';
+import Mock01 from './c_mock/Mock01';
+
+export default function Index() {
+  // Context API를 사용한 전역 상태관리
+  // ThemeContext를 사용하여 현재 테마 상태를 가져옴
+  const { theme } = useContext(ThemeContext);
+
+  // 현재 테마에 따라 배경색 설정
+  const backgroundColor = theme === 'lightblue' ? 'pink' : 'lightblue';
+  
+  return (
+    <div style={{ backgroundColor: backgroundColor }}>
+      <h1>0105 리액트 수업 자료</h1>
+      <h2>Context API</h2>
+      <ContextApi01 />
+
+      <h2>
+        Styled Components
+      </h2>
+      <Styled01 />
+
+      <h2>
+        Mock data
+      </h2>
+      <Mock01/>
+    </div>
+  )
+}
